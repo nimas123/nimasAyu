b1be2bef3a34770b57574a17eb5b187cab4c5570
nimas123 <134251429+nimas123@users.noreply.github.com>
2023-08-04 14:29:45 +0700

    Add files via upload

---
+++ ppn.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public interface ppn {
+    
+    public static final double PERSEN = 0.0;
+    
+    public double hitungHargaSetelahPpn(double harga);
+}
